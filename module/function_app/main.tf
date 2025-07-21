4cb681597a023e1191bc1e4d9117efbb57a9b9cb
Narina <pnarina@kpmg.co.ke>
2025-07-21 14:57:02 +0300

    Added function app and craetion of RG for PAmela

---
+++ module/function_app/main.tf
@@ -0,0 +1,35 @@
+resource "azurerm_storage_account" "sa" {
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "plan" {
+  name                = var.app_service_plan_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  kind                = "FunctionApp"
+  reserved            = true
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_function_app" "fa" {
+  name                       = var.function_app_name
+  location                   = var.location
+  resource_group_name        = var.resource_group_name
+  app_service_plan_id        = azurerm_app_service_plan.plan.id
+  storage_account_name       = azurerm_storage_account.sa.name
+  storage_account_access_key = azurerm_storage_account.sa.primary_access_key
+  version                    = "~4"
+  os_type                    = "linux"
+
+  site_config {
+    linux_fx_version = "Python|3.10"
+  }
+}
