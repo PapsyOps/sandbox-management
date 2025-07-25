e289b7d1f40202424c7d8c473c2a48b58f6bb767
Narina <pnarina@kpmg.co.ke>
2025-07-25 09:03:30 +0300

    use of data- coz reading resources not craeting

--- module/storage_account/main.tf
+++ module/storage_account/main.tf
@@ -1,12 +1,12 @@
 resource "azurerm_storage_account" "this" {
-  name                     = var.name
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  account_tier             = var.account_tier
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  account_tier        = var.account_tier
   #kind                     = var.kind
   account_replication_type = var.replication_type
 
-blob_properties {
+  blob_properties {
     versioning_enabled = true
   }
 
