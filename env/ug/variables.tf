1b529a0e9b6e2215651e5169066e49966f2c45ee
Narina <pnarina@kpmg.co.ke>
2025-09-08 14:48:56 +0300

    env/ug/variables.tf

--- env/ug/variables.tf
+++ env/ug/variables.tf
@@ -1,51 +1,24 @@
-# Resource group name
 variable "resource_group_name" {
-  description = "Name of the Resource Group to create"
-  type        = string
-  default     = "KE-RG-EUW-IDAS-Moodle"
+  type = string
 }
-
-# Azure location
-variable "location" {
-  description = "Azure region where resources will be deployed"
-  type        = string
-  default     = "westeurope"
-}
-
-# Storage account name (must be globally unique, 3-24 lowercase letters and numbers)
 variable "storage_account_name" {
-  description = "Globally unique name for the Storage Account"
-  type        = string
-  default     = "keugprdsaveeambackup0001"
+  type = string
+}
+variable "location" {
+  type    = string
+  default = "West Europe"
 }
 
-# Storage replication type
-variable "storage_replication_type" {
-  description = "Replication type for the Storage Account (LRS, GRS, RAGRS, ZRS)"
-  type        = string
-  default     = "GRS"
+variable "account_tier" {
+  type    = string
+  default = "Standard"
 }
 
-# Storage account tier
-variable "storage_account_tier" {
-  description = "Storage Account performance tier"
-  type        = string
-  default     = "Standard"
+variable "account_replication_type" {
+  type    = string
+  default = "LRS"
 }
 
-# Tags (map for flexibility)
 variable "tags" {
-  description = "A mapping of tags to assign to the resources"
-  type        = map(string)
-  default = {
-    Charge_Code             = "MFKE03"
-    ke-app-budget           = "1000"
-    ke-app-business-unit    = "CSU"
-    ke-app-cost-owner       = "jnyarumba@kpmg.co.ke"
-    ke-app-department       = "ITS"
-    ke-app-end-of-life-date = "1 year"
-    ke-app-environment      = "production"
-    ke-app-name             = "VEEAM"
-    ke-app-tech-contact     = "mlutaaya@kpmg.com"
-  }
+  type = map(string)
 }
