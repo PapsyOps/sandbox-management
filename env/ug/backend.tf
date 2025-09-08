8bad3cb1de81a5f74fd3a52c0e4e5045f606eeb0
Narina <pnarina@kpmg.co.ke>
2025-09-08 14:50:39 +0300

    Added env/ug/backend.tf

---
+++ env/ug/backend.tf
@@ -0,0 +1,10 @@
+# Stores the state
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "KE-RG-SandboxManagement"
+    storage_account_name = "tfstatestoragemanagement"
+    container_name       = "tfstate"
+    key                  = "ug/terraform.tfstate"
+  }
+}
