0cc2d12dd696923e8ea3362955576b425cbabc0d
Narina <pnarina@kpmg.co.ke>
2025-09-08 16:04:17 +0300

    WOE6

--- env/ug/dev.tfvars
+++ env/ug/dev.tfvars
@@ -9,7 +9,6 @@ tags = {
   "ke-app-tech-contact"     = "dampaire@kpmg.com"
   "ke-app-end-of-life-date" = "Never"
   "ke-app-backup"           = "Yes"
-  "ke-patch-priorty"        = "Low"
 
 }
 
--- env/ug/main.tf
+++ env/ug/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 module "ug_rg" {
-  source              = "../module/resource_group"
+  source              = "../../module/resource_group"
   resource_group_name = var.resource_group_name
   location            = var.location
   tags                = var.tags
@@ -11,11 +11,11 @@ module "ug_rg" {
 }
 
 module "ug_storage" {
-  source                   = "../module/storage_account"
-  storage_account_name     = var.storage_account_name
-  resource_group_name      = module.ug_rg.resource_group_name
-  location                 = var.location
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
-  tags                     = var.tags
+  source              = "../../module/storage_account"
+  name                = var.storage_account_name
+  resource_group_name = module.ug_rg.resource_group_name
+  location            = var.location
+  account_tier        = var.account_tier
+  replication_type    = var.account_replication_type
+  tags                = var.tags
 }
